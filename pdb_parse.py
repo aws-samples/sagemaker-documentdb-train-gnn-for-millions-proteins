e00b82c44dd9ba7844293e7475a08963eceea2e4
Zichen Wang <zichewan@amazon.com>
2021-11-24 11:41:34 -0500

    Added license headers

--- pdb_parse.py
+++ pdb_parse.py
@@ -1,5 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
+# SPDX-License-Identifier: Apache-2.0
 
 """
 Helpers for parsing protein structure files and generating contact maps.
--- src/config.py
+++ src/config.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 """Configs for model training """
 
 # training set ups:
